932ede62a5f66234dbf7af7a2829c91f653d96df
Piotr Nowakowski <piotrnowakowski@air-piotr-2.home>
2023-05-07 22:45:33 +0200

    Init

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # SimpleCalc
+
 Simple minimalistic calculator app made in GTK with python
+
+---
+
+## Setup
+
+First read [how to set up gtk4 for your operating system](https://www.gtk.org/docs/installations/)
+
+#### Using pipenv
+
+To setup environment for this app using pipenv run:
+
+```bash
+pipenv install
+```
+
+> Note that you should hav [pipenv](https://pipenv.pypa.io/en/latest/index.html) installed
+
+#### Using requirements.txt
+
+To setup environment for this app the classic way run:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Running
+
+You can run this app using command
+
+```bash
+python app.py
+```
---
+++ app.py
@@ -0,0 +1,63 @@
+import sys
+import gi
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk
+
+class Calculator(Gtk.Window):
+    def __init__(self, app):
+        super().__init__(application=app, title="SimpleCalc")
+        # self.set_border_width(10)
+
+        # Create the grid
+        grid = Gtk.Grid()
+        self.set_child(grid)
+
+        # Create the entry field
+        self.entry = Gtk.Entry()
+        grid.attach(self.entry, 0, 0, 4, 1)
+
+        # Create buttons
+        buttons = [
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            '0', '.', '=', '+'
+        ]
+
+        x = 0
+        y = 1
+        for button_label in buttons:
+            button = Gtk.Button(label=button_label)
+            button.connect('clicked', self.button_clicked)
+            grid.attach(button, x, y, 1, 1)
+            x += 1
+            if x > 3:
+                x = 0
+                y += 1
+
+    def button_clicked(self, button):
+        # Get the current text from the entry field
+        expression = self.entry.get_text()
+
+        # Get the label text of the button clicked
+        label = button.get_label()
+
+        # Handle different button actions
+        if label == '=':
+            try:
+                result = eval(expression)
+                self.entry.set_text(str(result))
+            except Exception as e:
+                self.entry.set_text("Error")
+        else:
+            self.entry.set_text(expression + label)
+
+
+scalc = Gtk.Application(application_id='com.piotreknow02.SimpleCalc')
+def activate(app):
+    win = Calculator(app)
+    win.present()
+
+# Run the app
+scalc.connect('activate', activate)
+scalc.run(sys.argv)
